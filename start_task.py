ebe22fcf3d468cbb82cf459226fff2949b5bf5cb
zhangxiaolong <zhangxiaolong6@xiaomi.com>
2020-06-06 07:15:45 +0800

    modify start_task
    Signed-off-by: zhangxiaolong <zhangxiaolong6@xiaomi.com>

--- start_task.py
+++ start_task.py
@@ -3,23 +3,27 @@
 import sched
 import time
 
+from com_zxl_common import LogUtils
 from com_zxl_request.QsbkRequest import QsbkTxtRequest
 
 s = sched.scheduler(time.time, time.sleep)
 
 
 def start():
+    LogUtils.i("star_task::start")
     s.enter(3600, 1, start_qsbk_hot_pic_spider, ())
     s.run()
 
 
 def start_qsbk_hot_pic_spider():
-    # request = QsbkTxtRequest()
-    # request.start_task()
+    LogUtils.i("star_task::start_qsbk_hot_pic_spider")
+    request = QsbkTxtRequest()
+    request.start_task()
     start()
 
 
 if __name__ == '__main__':
+    LogUtils.i("star_task::main")
     start()
     # qsbkRequest = QsbkTxtRequest()
     # qsbkRequest.start_task()
